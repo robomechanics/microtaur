24cefa7782e1635a83fc67b0d9fc0263998fbfd0
Ankit Bhatia <ankitb@cmu.edu>
2021-11-05 17:35:16 -0400

    updated motor class to use the multi-threaded iq serial libr

--- Firmware/microtaur/include/motor.h
+++ Firmware/microtaur/include/motor.h
@@ -1,43 +1,49 @@
 #pragma once
 
-#include<iq_module_communication.hpp>
+#include <iq_module_communication.hpp>
+#include <iq/iq_serial_multithread.hpp>
 
-typedef enum {
+typedef enum
+{
     no_change = -1,
-    brake, 
-    coast, 
-    pwm, 
+    brake,
+    coast,
+    pwm,
     volts,
     velocity,
     angle,
     trajectory
 } ctrl_mode_t;
 
-class Motor{
-    private:
-        IqSerial ser_;
-        MultiTurnAngleControlClient client_;
-        
-    public:
-        Motor(HardwareSerial serial): ser_(serial), client_(0){}
-        
-        
-        void setCoast(){
-            ser_.set(client_.ctrl_coast_);
-        }
-        void sendTrajectorySpeed(double speed_cmd, double angle_cmd){
-            ser_.set(client_.trajectory_angular_displacement_, angle_cmd);
-            ser_.set(client_.trajectory_average_speed_, speed_cmd);
-        }
-        
-        void sendTrajectoryTime(double time_cmd, double angle_cmd){
-            ser_.set(client_.trajectory_angular_displacement_, angle_cmd);
-            ser_.set(client_.trajectory_duration_, time_cmd);
-        }
-        
-        ctrl_mode_t getMode(){
-            ctrl_mode_t mode=0;
-            ser_.get(client_.ctrl_mode_, mode);
-            return mode;
-        }
+class Motor
+{
+private:
+    IqSerialMultithreaded ser_;
+    MultiTurnAngleControlClient client_;
+
+public:
+    Motor(HardwareSerial serial) : ser_(serial), client_(0) {}
+
+    void setCoast()
+    {
+        ser_.set(client_.ctrl_coast_);
+    }
+    void sendTrajectorySpeed(double speed_cmd, double angle_cmd)
+    {
+        ser_.set(client_.trajectory_angular_displacement_, angle_cmd);
+        ser_.set(client_.trajectory_average_speed_, speed_cmd);
+    }
+
+    void sendTrajectoryTime(double time_cmd, double angle_cmd)
+    {
+        ser_.set(client_.trajectory_angular_displacement_, angle_cmd);
+        ser_.set(client_.trajectory_duration_, time_cmd);
+    }
+
+    ctrl_mode_t getMode()
+    {
+        ctrl_mode_t mode = 0;
+        ser_.get(client_.ctrl_mode_, mode);
+        return mode;
+    }
 }
